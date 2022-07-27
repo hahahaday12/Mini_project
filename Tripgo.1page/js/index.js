214c39b95c43a439b0808d70d338ec604bd85b88
hahahaday12 <101441685+hahahaday12@users.noreply.github.com>
2022-07-28 01:29:05 +0900

    Delete Tripgo.1page directory

--- Tripgo.1page/js/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const eventHandler = (src) => {
-  const imgList = document.querySelectorAll('.backImage');
-  for (let i = 0; i < imgList.length; i++) {
-    imgList[i].classList.remove('active');
-  }
-  document.querySelector(`.${src}`).classList.add('active');
-};
